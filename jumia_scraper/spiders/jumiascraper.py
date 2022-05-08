6863048bdad18a25ce8c34526aec8bff625747b7
Doro <sirdoro1@gmail.com>
2022-05-08 11:41:42 +0100

    update on spider

--- jumia_scraper/spiders/jumiascraper.py
+++ jumia_scraper/spiders/jumiascraper.py
@@ -17,10 +17,18 @@ class JumiascraperSpider(scrapy.Spider):
                     'product_url': 'https://jumia.com.ng'+products.attrib['href'],
                     'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0,
                 }
-        # response xpath for pagination
-        # the href attribute of the next page if it exists
-        next_page_url = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]').attrib['href']
-        next_page = 'https://jumia.com.ng'+ next_page_url
-        if next_page_url and next_page:
-            yield response.follow(next_page, callback=self.parse)
+        # last_page = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[7]').attrib['href']
+        # # extract page number '16' from the '/catalog/?q=oraimo&page=16#catalog-listing' and remove the '#catalog-listing'
+        # # remove all characters expect "page=" and extract the number
+        # page_number = last_page.split('page=')[1].split('#')[0].split('&')[0]
+        # # loop throuhg the pages and yield the next page
+        # for page in range(2, int(page_number)+1):
+        #     next_page = f'https://jumia.com.ng/catalog/?q=oraimo&page={page}/#catalog-listing'
+        #     yield response.follow(next_page, callback=self.parse)
+
+        next_page = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]')
+        # check if next page has a href attribute
+        if next_page and next_page.attrib['href']:
+            next_page_url = 'https://jumia.com.ng' + next_page.attrib['href']
+            yield response.follow(next_page_url, callback=self.parse)
 
