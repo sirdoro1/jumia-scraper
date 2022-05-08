e139b497ed569134d973de6d56fc69882a1fb7a5
Doro <sirdoro1@gmail.com>
2022-05-08 11:47:48 +0100

    update on spider

--- jumia_scraper/spiders/jumiascraper.py
+++ jumia_scraper/spiders/jumiascraper.py
@@ -13,7 +13,7 @@ class JumiascraperSpider(scrapy.Spider):
                     'product_name': products.css('h3.name::text').get(),
                     'product_image_url': products.css('img.img').attrib['data-src'],
                     # check if ₦ is in the price string and remove it else return the price
-                    'product_price': products.css('div.prc::text').get().replace('₦', '') if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
+                    'product_price': products.css('div.prc::text').get().replace('₦', '').strip() if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
                     'product_url': 'https://jumia.com.ng'+products.attrib['href'],
                     'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0,
                 }
