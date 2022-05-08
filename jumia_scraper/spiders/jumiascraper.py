32f60de7f6a3e0cd61f0862264c23664ee5db268
Doro <sirdoro1@gmail.com>
2022-05-08 11:03:00 +0100

    update on spider

--- jumia_scraper/spiders/jumiascraper.py
+++ jumia_scraper/spiders/jumiascraper.py
@@ -7,17 +7,20 @@ class JumiascraperSpider(scrapy.Spider):
 
     def parse(self, response):
         for products in response.css('a.core'):
-            self.log(message=len(products))
+            # self.log(message=len(products))
             if(products.css('h3.name::text').get() and products.css('div.prc::text').get()):
                 yield {
                     'product_name': products.css('h3.name::text').get(),
                     'product_image_url': products.css('img.img').attrib['data-src'],
-                    'product_price': products.css('div.prc::text').get(),
+                    # check if ₦ is in the price string and remove it else return the price
+                    'product_price': products.css('div.prc::text').get().replace('₦', '') if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
                     'product_url': 'https://jumia.com.ng'+products.attrib['href'],
-                    # 'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '')
+                    'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0,
                 }
         # response xpath for pagination
-        next_page = 'https://jumia.com.ng'+response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]').attrib['href']
-        if next_page:
+        # the href attribute of the next page if it exists
+        next_page_url = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]').attrib['href']
+        next_page = 'https://jumia.com.ng'+ next_page_url
+        if next_page_url and next_page:
             yield response.follow(next_page, callback=self.parse)
 
