21b0de66530b786ba06df53baa772ae76935e5c7
Doro <sirdoro1@gmail.com>
2022-05-08 10:36:08 +0100

    update on spider

--- jumia_scraper/spiders/jumiascraper.py
+++ jumia_scraper/spiders/jumiascraper.py
@@ -3,8 +3,21 @@ import scrapy
 
 class JumiascraperSpider(scrapy.Spider):
     name = 'jumiascraper'
-    allowed_domains = ['jumia.com.ng']
-    start_urls = ['http://jumia.com.ng/']
+    start_urls = ['http://jumia.com.ng/catalog/?q=oraimo']
 
     def parse(self, response):
-        pass
+        for products in response.css('a.core'):
+            self.log(message=len(products))
+            if(products.css('h3.name::text').get() and products.css('div.prc::text').get()):
+                yield {
+                    'product_name': products.css('h3.name::text').get(),
+                    'product_image_url': products.css('img.img').attrib['data-src'],
+                    'product_price': products.css('div.prc::text').get(),
+                    'product_url': 'https://jumia.com.ng'+products.attrib['href'],
+                    # 'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '')
+                }
+        # response xpath for pagination
+        next_page = 'https://jumia.com.ng'+response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]').attrib['href']
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
